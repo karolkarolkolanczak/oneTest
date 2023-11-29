ba721a23023449575a333b6c648a9781de70f55f
karolkarolkolanczak <karol.kolanczak@gmail.com>
2023-11-29 19:37:22 +0100

    init

---
+++ src/test/java/com/example/startone/StartOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
